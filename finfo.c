b0b3c7bcbaeb6e7c04db5ae58377450fdff01275
Matthew <sennm@msoe.edu>
2024-11-15 13:10:51 -0600

    Lab8 Completed

--- finfo.c
+++ finfo.c
@@ -1,9 +1,60 @@
-// Name, etc
+/**
+ * @file info.c
+ * @brief gets info about a provided file on the command line
+ * 
+ * Course: CPE2600
+ * Section: 011
+ * Assignment: Lab 9
+ * Name: Matthew Senn
+ */
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
 
-// finfo.c
+/**
+ * File Type                    X
+ * Permissions                  X
+ * Owner of the file--id        X
+ * file size                    X
+ * date of last modification    X
+ */
 
 int main(int argc, char* argv[])
-{
+{   
+    if(argc < 2){
+        printf("Insufficient arguments, exiting!\n");
+        return -1;
+    }
+    struct stat stat_buf;
+    if(stat(argv[1],&stat_buf)){
+        printf("Could not get file %s info. Exiting!\n",argv[1]);
+        return -1;
+    }
+    int mode = stat_buf.st_mode;
+    printf("%d\n",mode);
+    if (S_ISREG(mode)) printf("Regular file\n");
+    else if (S_ISDIR(mode)) printf("Directory\n");
+    else if (S_ISCHR(mode)) printf("Character device\n");
+    else if (S_ISBLK(mode)) printf("Block device\n");
+    else if (S_ISFIFO(mode)) printf("FIFO (named pipe)\n");
+    else if (S_ISLNK(mode)) printf("Symbolic link\n");
+    else if (S_ISSOCK(mode)) printf("Socket\n");
 
+    printf("File permissions: ");
+    if(S_IRUSR & mode)printf("Read | ");  
+    if(S_IWUSR & mode)printf("Write | ");
+    if(S_IXUSR & mode)printf("Exectue | ");
+    if(S_IRGRP & mode)printf("Read Group | ");
+    if(S_IWGRP & mode)printf("Write Group | ");
+    if(S_IXGRP & mode)printf("Execute Group | ");
+    if(S_IROTH & mode)printf("Read Other | ");
+    if(S_IWOTH & mode)printf("Write Other | ");
+    if(S_IXOTH & mode)printf("Execute Other | ");
+    printf("\n");
+
+    printf("Owner: %u\n",stat_buf.st_uid);
+    printf("Size: %ld\n",stat_buf.st_size);
+    printf("Modified at: %s", ctime(&stat_buf.st_mtime));
     return 0;
 }
--- pinfo.c
+++ pinfo.c
@@ -1,9 +1,55 @@
-// Name, etc
+/**
+ * @file pinfo.c
+ * @brief File to get information about the current or specified process
+ * 
+ * Course: CPE2600
+ * Section: 011
+ * Assignment: Lab 9
+ * Name: Matthew Senn
+ */
 
-// pinfo.c
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <sched.h>
+#include <unistd.h> 
+#include <sys/resource.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
+    int pid;
+
+    if (argc > 1) {
+        char *err;
+        pid = strtol(argv[1], &err, 10); // Convert to int
+        if (*err != '\0') { // Check if conversion was successful
+            printf("Invalid Process ID\n");
+            exit(1);
+        }
+    } else {
+        pid = getpid(); // Get current process ID
+    }
+
+    printf("Current process PID: %d\n", pid); 
+
+    int priority = getpriority(PRIO_PROCESS, pid);
+    printf("Priority: %d\n", priority);
+
+    int schedule = sched_getscheduler(pid);
+    printf("Policy: ");
+    if (schedule == SCHED_OTHER) {
+        printf("SCHED_OTHER\n");
+    } else if (schedule == SCHED_FIFO) {
+        printf("SCHED_FIFO\n");
+    } else if (schedule == SCHED_RR) {
+        printf("SCHED_RR\n");
+    } else if (schedule == SCHED_BATCH) {
+        printf("SCHED_BATCH\n");
+    } else if (schedule == SCHED_IDLE) {
+        printf("SCHED_IDLE\n");
+    } else {
+        printf("Unknown scheduling policy\n");
+    }
 
     return 0;
 }
